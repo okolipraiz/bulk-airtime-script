b02f43eaea53f280519acb82818ec46d37a324cd
okolipraiz <okolipraiz@gmail.com>
2023-02-02 23:05:51 +0100

    script complete

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
-import axios from "axios";
+import fetch from "node-fetch";
 import { phoneNumber } from "./data/contact.js";
 import FLUTTERWAVE_API_KEY from "./env.js";
 
+let promiseArray = [];
 for (const key in phoneNumber) {
     const amount = 100;
     const randomReferenceID = Math.floor(Math.random() * 1000000000000);
-    // console.log( phoneNumber[key], amount, randomReferenceID);
 
     var data = JSON.stringify({
-        bulk_reference: "edf-12de5223d2f32",
-        callback_url:
-            "https://webhook.site/5f9a659a-11a2-4925-89cf-8a59ea6a019a",
-        bulk_data: [
-            {
-                country: "NG",
-                customer: phoneNumber[key],
-                amount: amount,
-                recurrence: "ONCE",
-                type: "AIRTIME",
-                reference: randomReferenceID,
-            },
-        ],
+        country: "NG",
+        customer: phoneNumber[key],
+        amount: amount,
+        recurrence: "ONCE",
+        type: "AIRTIME",
+        reference: randomReferenceID,
     });
 
-    var config = {
-        method: "post",
-        url: "https://api.flutterwave.com/v3/bulk-bills",
-         headers: {
-             Authorization:
-                 `Bearer ${FLUTTERWAVE_API_KEY}`,
-             "Content-Type": "application/json",
-         },
-        data: data,
-    };
+    let request = fetch("https://api.flutterwave.com/v3/bills", {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${FLUTTERWAVE_API_KEY}`,
+            "Content-Type": "application/json",
+        },
+        body: data,
+    });
 
-    axios(config)
-        .then(function (response) {
-             console.log((response.data.data));
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    promiseArray.push(request);
 }
 
-
+Promise.all(promiseArray).then((values) => {
+    let jsonPromise = values.map((i) => i.json());
+    Promise.all(jsonPromise).then((jsonPromise) => {
+        console.log(jsonPromise);
+    });
+});
